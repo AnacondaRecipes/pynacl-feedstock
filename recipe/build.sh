93671195af5808dd0fc05a2231df1e9fedc16be8
anaconda-pkg-build <66446536+anaconda-pkg-build@users.noreply.github.com>
2022-08-04 08:43:39 -0500

    pynacl 1.5.0 (#3)
    * DOR update to 1.5.0
    
    * Skip py<36, update dependencies, add pip check

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/libsodium/build-aux
+
 if [[ ${HOST} =~ .*linux.* ]] && [[ ${ARCH} == 32 ]]; then
     export CFLAGS="$CFLAGS -Og"
 fi
+
+# Define the environment variable LIBSODIUM_MAKE_ARGS to pass arguments to make and enable parallelization
+export LIBSODIUM_MAKE_ARGS="-j4"
+
 export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
 export SODIUM_INSTALL="system"
+
 python -m pip install . --no-deps --ignore-installed
