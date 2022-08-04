7b2201edf13c7e85d0d89824a498f66488a9e40d
Serhii Kupriienko <skupriienko@anaconda.com>
2022-08-04 10:33:30 +0000

    Add faster wheel build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,9 @@ if [[ ${HOST} =~ .*linux.* ]] && [[ ${ARCH} == 32 ]]; then
     export CFLAGS="$CFLAGS -Og"
 fi
 
+# Define the environment variable LIBSODIUM_MAKE_ARGS to pass arguments to make and enable parallelization
+export LIBSODIUM_MAKE_ARGS="-j4"
+
 export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
 export SODIUM_INSTALL="system"
 
