d4857af5cb0df93847f68a1fe80f2b188faed086
Valentyna Khomits <vkhomits@anaconda.com>
2025-09-16 14:44:19 +0300

    Rebuild agains libsodium 1.0.20

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 rem https://github.com/pyca/pynacl/issues/165
+set PYNACL_SODIUM_LIBRARY_NAME=sodium
 set SODIUM_INSTALL=system
-set PYNACL_SODIUM_STATIC=1
+
 python -m pip install . --no-deps --ignore-installed --no-build-isolation
 if errorlevel 1 exit 1
