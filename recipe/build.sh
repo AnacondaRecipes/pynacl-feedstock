e3b57f146205d8ec38d48159428e167cefdc2d54
vkhomits <vkhomits@anaconda.com>
2025-09-17 09:50:09 +0300

    Merge pull request #7 from AnacondaRecipes/PKG-9566-pynacl-1.5.0-rebuild
    Rebuild agains libsodium 1.0.20

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,4 +13,4 @@ export LIBSODIUM_MAKE_ARGS="-j4"
 export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
 export SODIUM_INSTALL="system"
 
-python -m pip install . --no-deps --ignore-installed
+python -m pip install . --no-deps --no-build-isolation --ignore-installed
