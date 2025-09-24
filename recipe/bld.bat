dd8e6c849b7eadf3d912908f4f274ecb4076a8c4
Ivan Iziumov <iiziumov@anaconda.com>
2025-09-24 10:41:41 +0200

    pynacl 1.6.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 rem https://github.com/pyca/pynacl/issues/165
 set PYNACL_SODIUM_LIBRARY_NAME=sodium
 set SODIUM_INSTALL=system
-
-python -m pip install . --no-deps --ignore-installed --no-build-isolation
+set PYNACL_SODIUM_STATIC=1
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
 if errorlevel 1 exit 1
